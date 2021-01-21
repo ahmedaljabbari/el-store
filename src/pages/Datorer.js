7f982f4bd64d0984ea0c450a19680b8c8b856c1d
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-01-21 17:36:38 +0100

    Datorer component grid && list call test

--- src/pages/Datorer.js
+++ src/pages/Datorer.js
@@ -1,12 +1,31 @@
 import {React, Component} from "react";
+import Database from "../Database.json";
+import "../assets/datorer.css"
 
 class Datorer extends Component{
+  state={
+    items: Database
+  }
 
 
   render(){
     return(
       <div>
-        Datorer Page
+        <h2>Datorer</h2>
+        <div className="datorer-container">
+          
+            {this.state.items.map((item, index) =>
+              {
+                return <div key={index}>
+                  <img src={item.pic}></img>
+                  <h4>{item.name}</h4>
+                  <div>{item.about}</div>
+                </div>
+              }
+            )}
+          
+
+        </div>
       </div>
     )
   }
